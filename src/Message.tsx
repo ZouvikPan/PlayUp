1311a52b74b2d1b6599bcf905bf4b87d87c145db
ZouvikPan <souvik.pan2001@gmail.com>
2023-04-10 22:00:18 +0530

    Explored interfaces, children prop passing, useState Hooks in Typescript

---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+function Message (){
+    const name = 'Souvik'
+    return <h1> Hello {name}!</h1>
+}
+
+export default Message;
---
+++ src/components/Alert.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from 'react'
+
+interface AlertProps{
+    children: ReactNode;
+    onClose: () => void;
+}
+
+const Alert = ({children, onClose}: AlertProps) => {
+  return (
+    <div className="alert alert-primary alert-dismissible" role="alert">
+      {children}
+      <button type="button" className="btn-close" onClick={onClose} data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+  )
+}
+
+export default Alert
